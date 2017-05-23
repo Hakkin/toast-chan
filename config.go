9512d620ed7ae452deed583236619b734b43b57a
Hakkin Lain <hakkin@github>
2017-05-22 17:56:21 -0700

    Added config loading

---
+++ config.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type mainConfig struct {
+	Token string
+}
+
+// Loads JSON config into supplied struct, optionally panics on error
+// Config struct fields must be exported
+func loadConfig(configname string, configstruct interface{}, panicerr bool) error {
+	io, err := ioutil.ReadFile(configname + ".json")
+	if err != nil {
+		if panicerr {
+			panic(err) // TODO: Change to proper logging once made
+		}
+		return err
+	}
+	
+	err = json.Unmarshal(io, &configstruct)
+	if err != nil {
+		if panicerr {
+			panic(err) // TODO: Change to proper logging once made
+		}
+		return err
+	}
+	
+	return nil
+}
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
+	var config mainConfig
+	loadConfig("config", &config, true) // Load config, panic if it fails
+	
 	
 }
