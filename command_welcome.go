491cb2b3afc64f116cef07ea3393e0c899d0dd82
Hakkin Lain <hakkin@github>
2017-05-23 16:09:26 -0700

    Edited welcome message

--- command_welcome.go
+++ command_welcome.go
@@ -20,8 +20,8 @@ func welcomeUser(s *discordgo.Session, m *discordgo.GuildMemberAdd) {
 	}
 
 	s.ChannelMessageSend(config.Channels.GreetingChannel, fmt.Sprintf(
-		"Welcome to the ASTOST English Discord, <@%s> (｡＾＿＾｡)づ！！！\n"+
-			"To make sure we know who you are, please post a link to your ASTOST profile or let us know what your username is. Sometimes it takes us a while to check this so please be patient.\n"+
-			"If you need help with anything, @ one of the admins or moderators in the userlist and we'll get back to you as soon as possible.\n"+
+		"Welcome to the ASTOST English Discord <@%s> (｡＾＿＾｡)づ！！！\n\n"+
+			"To make sure we know who you are, please post a link to your ASTOST profile or let us know what your username is. Sometimes it takes us a while to check this so please be patient.\n\n"+
+			"If you need help with anything, @ one of the admins or moderators in the userlist and we'll get back to you as soon as possible.\n\n"+
 			"Once again, welcome to ATOAST! ＼(｡＾ ｏ ＾｡)／", m.User.ID))
 }
